aecc41623df1bb269a0216feec0c028ef568fd2a
Henrique R. Pohl <henriqpohl@MacBook-Pro.local>
2023-03-15 11:28:31 +0000

    Finalizando a aula: 1: Configurando Modal de criacao

--- README.md
+++ README.md
@@ -20,7 +20,19 @@ Instalando o Axios (Gerenciamento de requsicoes HTTP)
 
 - Terminal: yarn add axios
 
+Instalando a Biblioteca React Modal
 
+- Terminal: yarn add react-modal
+
+Anotacao sobre o a criacao do Modal
+
+Para fazer o modal utilizamos Lib React Modal, começamos utilizando o useState estando isNewTransactionOpen
+ e setIsNewTransactionOpen, fazemos 2 funções para abrir e fechar o modal (handleOpenNewTransactionModal
+ e handleCloseNewTransactionModal) que recebem o isNewTransactionOpen True ou False.
+
+Dentro do template Modal, usamos a tag isOpen para chamar a função isNewTransactionOpen para abrir o modal e a tag onRequestClose recebe HandleCloseNewTransaction para fechar o modal. 
+
+Neste caso, o Modal fica no header, para deixaremos ele funcionando na raiz do documento, ou seja no App, para isso, jogaremos todas as funções criadas no header e também o template de modal para a raiz (App) e de la, iremos receber a função via propriedade (props)
 
 
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,43 @@
-import styled from 'styled-components';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
+import { useState } from 'react';
+import Modal from 'react-modal';
 
+//import styled from 'styled-components';
 import { GlobalStyle } from './styles/global';
 
-const Title = styled.h1`
-  font-size: 64px;  
-  color: #8257e6;
-`
+// const Title = styled.h1`
+//   font-size: 64px;  
+//   color: #8257e6;
+// `
+
+Modal.setAppElement('#root');
 
 export function App() {
+
+  const [isNewTransactionOpen, setIsNewTransactionOpen] = useState(false);
+
+    function handleOpenNewTransactionModal() {
+        setIsNewTransactionOpen(true);
+    }
+
+    function handleCloseNewTransactionModal() {
+        setIsNewTransactionOpen(false);
+    }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <Modal
+        isOpen={isNewTransactionOpen}
+        onRequestClose={handleCloseNewTransactionModal}>
+                
+        <h2>Cadastrar Transacao</h2>
+               
+      </Modal>
+
       <GlobalStyle />
     </>
   );
